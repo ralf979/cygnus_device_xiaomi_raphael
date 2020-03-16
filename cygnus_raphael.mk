6470784f388f8974f9539a73d748d05672ddf8a9
ralf979 <ralf979@gmail.com>
2020-03-16 12:37:20 +0100

    cygnus-caf_wip

---
+++ cygnus_raphael.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+$(call inherit-product, vendor/cygnus/configs/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := raphael
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := cygnus_raphael
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
