9701ad991a79a799e69d33303c37d09887d125d6
ralf979 <ralf979@gmail.com>
2020-03-18 23:17:38 +0100

    add overlay cygnus

--- msmnile.mk
+++ msmnile.mk
@@ -38,7 +38,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-cygnus
 
 # Properties
 -include $(LOCAL_PATH)/device_props.mk
